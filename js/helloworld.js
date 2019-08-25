574901b62a9a8fa8728a267990692051dcd30278
soompy <sooper1137@gmail.com>
2019-08-25 23:23:28 +0900

    페이징 효과 넣기

--- js/helloworld.js
+++
@@ -1,3 +0,0 @@
-$("#alert-target").click(function () {
-    toastr["info"]("I was launched via jQuery!")
-});
--- js/renewal.js
+++ js/renewal.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
-    modal();
-    range();
+    //modal();
+    //range();
+    paging();
+    moving();
 });
 
 
@@ -24,3 +26,25 @@ function range() {
         " - $" + $("#slider-range").slider("values", 1));
 }
 
+function paging() {
+    var $li = $(".pagination li");
+    $(".pagination li a").removeClass();
+    $li.on("click", function () {
+        $li.children("a").removeClass();
+        $(this).children("a").addClass("active");
+    })
+}
+
+function moving() {
+    var $roomList = $(".room_list");
+    var $prev = $(".prev");
+    var $next = $(".next");
+
+    $prev.on("click", function () {
+        $roomList.css("marginLeft", "-1000px").animate({left:0},500);
+    })
+
+    $next.on("click", function () {
+        $roomList.css("left", "+1000px");
+    })
+}
