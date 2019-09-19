85bfd4c70d0572ed2f8deebb21b2bb0bd93efa1c
soompy <sooper1137@gmail.com>
2019-09-19 17:54:00 +0900

    이미지 스와이프 research

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,5 @@
 function ourService(evt, serviceName) {
-    var i, tabcontent, tablinks;
+    var i, tabcontent, tablinks, item;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -8,6 +8,16 @@ function ourService(evt, serviceName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+    item = document.getElementsByClassName("item");
+    for (i = 0; i < item.length; i++) {
+        item[i].className = item[i].className.replace(" on", "");
+        item[i].style.display = "block";
+        item[i].style.left = "126px";
+        item[i].style.zIndex = "20";
+    }
+
     document.getElementById(serviceName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+
