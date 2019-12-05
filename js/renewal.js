ac69b86ed32fbc81b8776b341e09d0edaa9bbac9
soompy <sooper1137@gmail.com>
2019-12-05 16:33:58 +0900

    직방 마크업

--- js/renewal.js
+++ js/renewal.js
@@ -1,21 +1,20 @@
 $(document).ready(function () {
     gnb();
-    //range();
     typeTab();
     paging();
-    mobile();
+    listBtn();
+    mGnb();
 });
 
 
 
 function gnb() {
-    $(".gnb_s").removeClass("on");
-    // $(this).siblings(".gnb_s").removeClass("on");
     $(".gnb_area_bg").removeClass("on");
-
+    $(".gnb_s").removeClass("on");
+    
     $(".gnb_li > a").hover(function(e){
         $(".gnb_area_bg").addClass("on");
-        $(this).siblings(".gnb_s").addClass("on");
+        $(this).next(".gnb_s").addClass("on");
     })
 
     $(".header_lst_area").mouseleave(function(e){
@@ -26,16 +25,18 @@ function gnb() {
 
 
 function typeTab() {
+    var  cnt = 0;
+    $(".search_box input").hide().first().show();
     $(".type_tab li").removeClass("active");
-    $(".search_box input")
 
     $(".type_tab li").hover(function(e){
+        cnt = $(this).index();
         $(this).addClass("active");
+        $(".search_box input").hide().eq(cnt).show();
     })
     $(".type_tab li").mouseleave(function(e){
         $(this).removeClass("active");
     })
-
 }
 
 
@@ -58,9 +59,39 @@ function range() {
 
 function paging() {
     var $li = $(".pagination li");
-    $(".pagination li a").removeClass();
+    $(".pagination li a").removeClass().first().addClass("active");
     $li.on("click", function () {
         $li.children("a").removeClass();
         $(this).children("a").addClass("active");
+    })
+}
+
+
+
+function listBtn() {
+    var first = "";
+    var last = $(".room_list li").last();
+
+    $(".sell_list_btn .prev span").on("click",function(e){     
+        $(".room_list").css("left","+=193px");
+        $(".room_list").prepend(last);
+        $(".room_list").css("left","0");
+
+    })
+    $(".sell_list_btn .next span").on("click",function(e){
+        first = $(".room_list li").first();
+        $(".room_list").css("left","-=193px");
+        $(".room_list").append(first);
+        $(".room_list").css("left","0");
+    })
+}
+
+
+function mGnb(){
+    $(".hamburger1").click(function(){
+        $(".overlay").addClass("overlay_show");
+        $(".hamburger1").click(function(){
+            $(".overlay").removeClass("overlay_show");
+        })
     })
 }
--- js/zb_effect.js
+++ js/zb_effect.js
@@ -1,41 +1,9 @@
 (function () {
-    // 헤더
-    // .gnb_li에 hover했을때, 
-    // .gnb_area_bg와 해당 gnb_s가
-    // 나타나도록 visibility: visible;값을
-    // 준다.
-    var gnbLi = document.querySelectorAll('.gnb_li');
-    var gnbS = document.querySelectorAll('.gnb_s');
-    var gnbBg = document.querySelector('.gnb_area_bg');
+    var overlay = document.getElementById('overlay');
+    var burger = document.getElementById('hamburger1');
 
-    gnbLi.forEach(hover => {
-        if (hover) {
-            gnbBg.style.visibility = 'visible';
-        } else {
-            gnbBg.style.visibility = 'hidden';
-        }
-    });
-
+    burger.addEventListener('click', function () {
+        overlay.classList.toggle('overlay_show');
+    })
 })();
 
-
-(function (m_menu) {
-
-})();
-
-
-
-(function () {
-
-    // .type_tab li를 클릭하면 .type_tab li.active 클래스가 들어오면서 
-    // 동시에 .search_box input의 내용이 드러난다.
-
-})();
-
-
-(function () {
-
-    // .pag li a를 누르면 ++ --를 사용한다.
-    // .sell_list_btn li span을 누르면 ++ --된다.
-
-})();
